28a39694f37189b57d8c332ee66841640ec1bdb3
ssskram <paul.marks@pittsburghpa.gov>
2019-06-06 11:09:58 -0400

    wired store to home

--- app/src/components/home/index.tsx
+++ app/src/components/home/index.tsx
@@ -3,19 +3,31 @@ import { connect } from "react-redux";
 import { ApplicationState } from "../../store";
 import HydrateStore from "../utilities/hydrateStore";
 import { Container, Col, Row } from "react-bootstrap";
-import Form from "../demoForm"
+import * as guns from "../../store/guns";
+import * as user from "../../store/user";
+import * as types from "../../store/types";
+import Form from "../demoForm";
 
-const Home = () => {
+type props = {
+  guns: types.gun[];
+  user: types.user;
+};
+
+const Home = (props: props) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  useEffect(() => {
+    console.log(props);
+  });
 
   return (
     <Container>
       <HydrateStore />
       <Row>
         <Col md={{ span: 4, offset: 4 }}>
-          <Form/>
+          <Form />
         </Col>
       </Row>
     </Container>
@@ -23,6 +35,12 @@ const Home = () => {
 };
 
 export default connect(
-  (state: ApplicationState) => ({}),
-  {}
-)(Home as any);
+  (state: ApplicationState) => ({
+    ...state.user,
+    ...state.guns
+  }),
+  {
+    ...user.actionCreators,
+    ...guns.actionCreators
+  }
+)(Home);
--- app/src/components/utilities/hydrateStore.tsx
+++ app/src/components/utilities/hydrateStore.tsx
@@ -3,24 +3,29 @@ import { useEffect } from "react";
 import { connect } from "react-redux";
 import { ApplicationState } from "../../store";
 import * as user from "../../store/user";
+import * as guns from "../../store/guns";
 
 type props = {
   loadUser: () => void;
+  loadGuns: () => void;
 };
 
 const Hydrate = (props: props) => {
   useEffect(() => {
     props.loadUser();
-  });
+    props.loadGuns();
+  }, []);
 
   return null;
 };
 
 export default connect(
   (state: ApplicationState) => ({
-    ...state.user
+    ...state.user,
+    ...state.guns
   }),
   {
-    ...user.actionCreators
+    ...user.actionCreators,
+    ...guns.actionCreators
   }
 )(Hydrate);
